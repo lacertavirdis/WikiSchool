ceeb95353e93614cd296320997ceb8bfc11c19b3
lacertavirdis <zuzanna.przybyl@uczen.zsk.poznan.pl>
2024-01-06 15:21:01 +0100

    folders

---
+++ php/applications.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="applications.css?v=<?php echo time(); ?>">
+    <title>Applications</title>
+</head>
+<body>
+    <main>
+        <div>
+            <h1 id="ht">Teams</h1>
+            <h1 id="hl">Outlook</h1>
+        </div>
+        <div>
+            <h1 id="hd">OneDrive</h1>
+            <h1 id="hn">OneNote</h1>
+        </div> 
+    </main>
+    <script src="applications.js?v=<?php echo time(); ?>">
+    </script>
+</body>
+</html>
